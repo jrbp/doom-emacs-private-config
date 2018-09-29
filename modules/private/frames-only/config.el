a7360328bef62dde3d3fdd0e96d21e5d6107e339
jrbp <fakeemail728@gmail.com>
2018-09-29 11:42:02 -0400

    Initial commit

---
+++ modules/private/frames-only/config.el
@@ -0,0 +1,7 @@
+;;; private/frames-only/config.el -*- lexical-binding: t; -*-
+
+(def-package! frames-only-mode
+  :config
+  ;(require 'frames-only-mode)
+  (frames-only-mode) ;; for now we just enable it after loading (maybe in the future we'll bind something to toggle it)
+  )
---
+++ modules/private/frames-only/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; private/frames-only/packages.el
+
+(package! frames-only-mode)
