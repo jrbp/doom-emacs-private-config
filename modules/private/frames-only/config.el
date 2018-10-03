2b39ce793b294ee52dbb75aced5032dee8195ffe
jrbp <fakeemail728@gmail.com>
2018-10-03 00:11:00 -0400

    M-RET to open files in new frame with frames-only

--- modules/private/frames-only/config.el
+++ modules/private/frames-only/config.el
@@ -4,4 +4,21 @@
   :config
   ;(require 'frames-only-mode)
   (frames-only-mode) ;; for now we just enable it after loading (maybe in the future we'll bind something to toggle it)
+
+  (map! (:after helm
+          (:after helm-files
+            :map (helm-find-files-map helm-read-file-map)
+            [M-return] #'helm-ff-run-switch-other-frame)
+          (:after helm-locate
+            :map helm-generic-files-map
+            [M-return] #'helm-ff-run-switch-other-frame)
+          (:after helm-buffers
+            :map helm-buffer-map
+            [M-return] #'helm-buffer-switch-other-frame)
+          (:after helm-regexp
+            :map helm-moccur-map
+            [M-return] #'helm-moccur-run-goto-line-of)
+          (:after helm-grep
+            :map helm-grep-map
+            [M-return] #'helm-grep-run-other-frame-action)))
   )
