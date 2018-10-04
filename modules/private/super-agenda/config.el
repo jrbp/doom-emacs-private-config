260142775d5fff30523f8a3aba0987757b19ba63
jrbp <fakeemail728@gmail.com>
2018-10-04 11:37:47 -0400

    Fix super agenda evil bindings

--- modules/private/super-agenda/config.el
+++ modules/private/super-agenda/config.el
@@ -2,7 +2,20 @@
 
 (def-package! org-super-agenda
   :config
-  ;(setq org-super-agenda-mode 1) how to enable mode?
+  ; following defun makes headers dumb, but at least doesn't break evil bindings
+  (defun org-super-agenda--make-agenda-header (s)
+    "Return agenda header containing string S.
+Prepended with `org-super-agenda-header-separator'."
+    (pcase s
+      ('none "")
+      (_ (setq s (concat " " s))
+         (org-add-props s nil 'face 'org-agenda-structure)
+                        ;'keymap org-super-agenda-header-map
+                        ;;; NOTE: According to the manual, only `keymap' should be necessary, but in my
+                        ;;; testing, it only takes effect in Agenda buffers when `local-map' is set, so
+                        ;;; we'll use both.
+                        ;'local-map org-super-agenda-header-map)
+         (concat org-super-agenda-header-separator s))))
   (org-super-agenda-mode)
   (setq org-super-agenda-groups
          '((:auto-category t))))
