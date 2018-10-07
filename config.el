27443bd8ecda3818abfbdc0ed2e9db28e69ef9a0
jrbp <fakeemail728@gmail.com>
2018-10-07 12:47:25 -0400

    misc-log.org for capturing and clocking misc tasks

--- config.el
+++ config.el
@@ -86,6 +86,8 @@
  (setq org-capture-templates
        '(("t" "Todo" entry (file+headline "~/org/master.org" "Tasks")
           "* TODO %?\n  %i %a %U")
+         ("l" "Log (misc)" entry (file+headline "~/org/misc-log.org" "Tasks")
+          "* TODO %?\n  %i %a %U")
          ("s" "Someday" entry (file+headline "~/org/someday.org" "Tasks")
           "* TODO %?\n  %i %a %U")
          ("a" "Appointments" entry (file+headline "~/org/master.org" "Misc appointments")
