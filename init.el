aba8762ef91a8a04b855c2e3e96b048e8faa2977
jrbp <fakeemail728@gmail.com>
2018-10-07 10:01:47 -0400

    Enabling more latex features

--- init.el
+++ init.el
@@ -100,7 +100,9 @@
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
-       latex             ; writing papers in Emacs has never been so fun
+       (latex
+        +latexmk
+        +zathrua)    ; writing papers in Emacs has never been so fun
        ;;ledger            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
