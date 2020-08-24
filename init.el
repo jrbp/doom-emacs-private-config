228c9c0b672805a23ff4ddf29e2ab4330aef3b22
John Bonini <jbonini@c02cq0gmmd6v.localdomain>
2020-08-24 14:29:30 -0400

    add julia

--- init.el
+++ init.el
@@ -134,7 +134,7 @@
        ;;idris             ;
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
-       ;;julia             ; a better, faster MATLAB
+       julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        (latex
         +latexmk)    ; writing papers in Emacs has never been so fun
