e48b43a47f134933ae9faf87087b84cd6f492fb9
John Bonini <jrbp@user.noreply.github.com>
2021-03-23 17:21:12 -0400

    workaround for linked issue

--- config.el
+++ config.el
@@ -47,6 +47,13 @@
 (set-popup-rules!
   '(("^\\*jupyter.*" :ignore t)))
 
+; temp fix attachment bug invalid base64 data bug, see https://github.com/hlissner/doom-emacs/issues/3185
+(defadvice! no-errors/+org-inline-image-data-fn (_protocol link _description)
+  :override #'+org-inline-image-data-fn
+  "Interpret LINK as base64-encoded image data. Ignore all errors."
+  (ignore-errors
+    (base64-decode-string link)))
+
 ; make it so that by default ESC is sent to vterm
 (add-hook! 'vterm-mode-hook #'evil-collection-vterm-toggle-send-escape)
 
